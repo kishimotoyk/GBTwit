1636628e5e523c96cfb75d1ebe1aea6024413cf0
kishimoto <kissy0613@gmail.com>
2017-01-13 16:38:29 +0900

    このままだとエラー出る いったん保存

--- Test2-master/WindowsFormsApplication1/Form1.cs
+++ Test2-master/WindowsFormsApplication1/Form1.cs
@@ -11,6 +11,8 @@ using CoreTweet;
 
 //http://dobon.net/vb/dotnet/programing/displayprogress.html#threadcancel ここを試してみる
 
+//http://kuroeveryday.blogspot.jp/2013/04/ctwitteroauth.html OAuth認証のやつ
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
@@ -22,7 +24,9 @@ namespace WindowsFormsApplication1
         private NotifyIcon notifyIcon1;
         private System.ComponentModel.IContainer components;
         string s3;
+        private Button button2;
         int count = 0;
+
 
         public Form1()
         {
@@ -63,11 +67,12 @@ namespace WindowsFormsApplication1
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 38);
+            this.button1.Location = new System.Drawing.Point(124, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -107,11 +112,23 @@ namespace WindowsFormsApplication1
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "中止";
+            this.button2.Enabled = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 200);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -123,13 +140,13 @@ namespace WindowsFormsApplication1
 
         public async void button1_Click(object sender, EventArgs e)
         {
-            if (count == 0) {
-                count++;
             notifyIcon1.BalloonTipTitle = "guraburugo";
             notifyIcon1.BalloonTipText = "監視を開始しました";
             notifyIcon1.ShowBalloonTip(3000);
-            button1.Text = "中止";
+            button1.Enabled = false;
+            button2.Enabled = true;
             //Task.Run(
+           
                
             CoreTweet.Tokens tokens = CoreTweet.Tokens.Create("ewtg8suISRPzfZyxWEKwTDoQf"
                 , "SoOOnaoylZnjLJu8n4ygMyENW2RQ5QFq1IjdfuUp5A9blr9oKK"
@@ -137,7 +154,8 @@ namespace WindowsFormsApplication1
                , "Xy6yeEU6seLDBvx8UgoEemBXsSY5vZD0kV1WdAFpqtSZl");
             for (int g = 0; g <= 10000; g++)
             {
-              
+
+               
                 string ret = await AsyncTask();
                 /*  int index = comboBox1.SelectedIndex;
 
@@ -195,16 +213,12 @@ namespace WindowsFormsApplication1
                 notifyIcon1.BalloonTipText = s3;
                 notifyIcon1.ShowBalloonTip(3000);
             }
-            }
-            else if (count == 1)
-            {
-                
-            }
-                
             
         }
         public async Task<string> AsyncTask()
         {
+
+            
             CoreTweet.Tokens tokens = CoreTweet.Tokens.Create("ewtg8suISRPzfZyxWEKwTDoQf"
                     , "SoOOnaoylZnjLJu8n4ygMyENW2RQ5QFq1IjdfuUp5A9blr9oKK"
                   , "3167038009-tVOk9rSAdYIrdUbcp7LW29Rsidr44UttspFYDFZ"
@@ -216,6 +230,8 @@ namespace WindowsFormsApplication1
 
             Func<string> asyncJob = () =>
             {
+
+                
               
                 Thread.Sleep(5000);
                 foreach (var m in tokens.Streaming.Filter(track => Text)
@@ -241,6 +257,38 @@ namespace WindowsFormsApplication1
             
             string ret = await Task.Run(asyncJob);
             return ret;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            
+            button1.Enabled = true;
+
+        }
+
+       
+        //コントロールの値を変更する
+      /*  private void SetProgressValue(int num)
+        {
+            //ProgressBar1の値を変更する
+            ProgressBar1.Value = num;
+            //Label1のテキストを変更する
+            Label1.Text = num.ToString();
+        }*/
+
+        //処理が完了した時にコントロールの値を変更する
+        private void ThreadCompleted()
+        {
+
+        }
+
+        //処理がキャンセルされた時にコントロールの値を変更する
+        private void ThreadCanceled()
+        {
+            label1.Text = "キャンセルされました。";
+            button1.Enabled = true;
+            button2.Enabled = false;
         }
 
     }
